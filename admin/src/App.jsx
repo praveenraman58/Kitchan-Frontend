09e420e49aaf02ca35f85743faf350ac31b18a30
Pravee <praveenraman58@gmail.com>
2024-08-27 10:57:40 +0530

    first commit

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-  const url = "http://localhost:5000";
+  const url = "https://kitchan-backend.onrender.com";
   return (
     <div>
       <ToastContainer />
